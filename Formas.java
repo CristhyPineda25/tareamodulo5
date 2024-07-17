960b479c99f91579599fc9b2c232914dd397d60e
CristhyPineda25 <pinedasoad25@gmail.com>
2024-07-17 06:43:18 +0200

    Add files via upload

---
+++ Formas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pined
+ */
+public class Formas {
+    private String color;
+
+    public Formas(String color) {
+        this.color = color;
+    }
+
+    public void establecerColor(String color) {
+        this.color = color;
+    }
+
+    public void dibujar() {
+       
+    }
+}
+
